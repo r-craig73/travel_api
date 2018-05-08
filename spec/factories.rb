f409db6f4eff9c8c3f1ba4c57d4044eb30f9092f
Kaila Goff and Ron Craig <kaila@>
2018-05-08 08:44:33 -0700

    passing validations for User model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  factory :user do
+    name "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
   factory(:location) do
     country('Mexico')
     city('Mexico City')
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe User do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+end
